e0a95018150dedb438de424011d66756b9ea9fbb
Sawwaty <yezhovye@mac.com>
2021-07-24 18:49:08 +0300

    UI elements setup

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
     }
-
-
 }
 
